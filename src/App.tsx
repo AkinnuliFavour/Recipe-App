3e802c1dc287d45f4099a9369dc83ac490ad77e0
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2023-02-07 06:27:09 +0100

    Basic structure of Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import { useState, useEffect } from "react";
+import api from "./api/meal";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export type stateType = {
+  id: string;
+  strMeal: string;
+  strMealThumb: string;
+};
 
 function App() {
-  const [meals, setMeals] = useState(0);
+  const [meals, setMeals] = useState<stateType[] | []>([]);
 
-  return <div className="App"></div>;
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await api.get("/filter.php?c=beef");
+        const mealArrays: stateType[][] = Object.values(response.data);
+        const [mealArray] = mealArrays;
+        setMeals(mealArray);
+        console.log(response.data);
+      } catch (err) {
+        if (err instanceof Error) {
+          // Not in the 200 response range
+          console.log(err);
+        } else {
+          console.log(`Error: ${String(err)}`);
+        }
+      }
+    };
+    fetchMeals();
+  }, []);
+
+  console.log(meals.map((m) => m.strMeal));
+
+  return (
+    <main className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home meals={meals} />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { stateType } from "./App";
+import Nav from "./components/Nav";
+import { Link } from "react-router-dom";
+
+const Home = ({ meals }: { meals: stateType[] }) => {
+  return (
+    <>
+      <Nav />
+      <main className="grid grid-cols-3 gap-6 p-4">
+        <>
+          {meals.map((meal) => (
+            <Link to={`/single-meal-page/${meal.id}`} key={meal.id}>
+              <figure>
+                <figcaption>{meal.strMeal}</figcaption>
+              </figure>
+            </Link>
+          ))}
+        </>
+      </main>
+    </>
+  );
+};
+
+export default Home;
